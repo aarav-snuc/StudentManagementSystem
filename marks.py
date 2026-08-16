df46e66cbfad7b2a984925b3e43668da91be18ff
Developer B <mail@aaravs.net>
2026-08-16 22:43:10 +0530

    Implemented marks module

--- marks.py
+++ marks.py
@@ -1,2 +1,13 @@
+marks_records = {}
+
+
 def add_marks(roll, subject, score):
-    pass
+    marks_records.setdefault(roll, {})[subject] = score
+    print("Marks added for", roll, ":", subject, "=", score)
+
+
+def average_marks(roll):
+    scores = marks_records.get(roll, {})
+    if not scores:
+        return 0
+    return sum(scores.values()) / len(scores)
